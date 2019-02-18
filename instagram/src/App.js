77b2013b77723928f767b584f5c3a8740b6aa866
James Basile <jbasile14@gmail.com>
2019-02-18 17:22:52 -0500

    init commit, class constructor complete

---
+++ instagram/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import dummyData from './dummy-data';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyDataArray: dummyData
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <SearchBar />
+        </div>
+        <div className="posts-container">
+        <PostsContainer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
