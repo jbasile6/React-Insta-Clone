a18fd4bae62776b6747618ad155825bf7e274752
James Basile <jbasile14@gmail.com>
2019-02-18 21:10:01 -0500

    deleted everything, starting from scratch

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -14,12 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <SearchBar />
-        </div>
-        <div className="posts-container">
-        <PostsContainer />
-        </div>
+        
       </div>
     );
   }
--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,24 +2,4 @@ import React from 'react';
 
 
 
-const SearchBar = () => {
-    return (
-        <div className="searchBar-container">
-
-            <div className="insta-logo-container">
-                <p>LOGO</p>
-            </div>
-
-            <div className="searchBar">
-                <p>SEARCHBAR</p>
-            </div>
-
-            <div className="icons">
-                <p>ICONS</p>
-            </div>
-        </div>
-    );
-};
-
-
 export default SearchBar;
